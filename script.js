c38b7f6244d0caa349e75755758bc81bc9cc3c4c
cmaongca09 <clarencedave.maongca@tup.edu.ph>
2023-11-04 14:05:30 +0800

    Updated index.html, script.js, style.css
    -added the infected contents again fixed the bug between survivors and infectedf section

--- script.js
+++ script.js
@@ -132,3 +132,117 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+var modal = document.getElementById("myModal6");
+var img = document.getElementById("CPI2");
+var modalImg = document.getElementById("CPI02");
+var captionText = document.getElementById("caption6");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[5];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("myModal7");
+var img = document.getElementById("CPI3");
+var modalImg = document.getElementById("CPI03");
+var captionText = document.getElementById("caption7");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[6];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("myModal8");
+var img = document.getElementById("CPI4");
+var modalImg = document.getElementById("CPI04");
+var captionText = document.getElementById("caption8");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[7];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("myModal9");
+var img = document.getElementById("CPI5");
+var modalImg = document.getElementById("CPI05");
+var captionText = document.getElementById("caption9");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[8];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("myModal10");
+var img = document.getElementById("CPI6");
+var modalImg = document.getElementById("CPI06");
+var captionText = document.getElementById("caption10");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[9];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("myModal11");
+var img = document.getElementById("CPI7");
+var modalImg = document.getElementById("CPI07");
+var captionText = document.getElementById("caption11");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+var span = document.getElementsByClassName("close")[10];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
