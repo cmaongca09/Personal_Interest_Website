984388139bac21672a424f3bbe0e35c05463dd3d
cmaongca09 <clarencedave.maongca@tup.edu.ph>
2023-11-05 00:15:42 +0800

    updated index.html mediaqueries, script.js style.css
    -overhaul rework of character section
    - change syntax
    - fixed column rows

--- script.js
+++ script.js
@@ -37,269 +37,40 @@ weaponsArrow.onclick = function () {
   navLinks.classList.toggle("show3");
 };
 
-// Get the modal
-var modal = document.getElementById("myModal");
-var img = document.getElementById("CS1");
-var modalImg = document.getElementById("CS01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// function myFunction(event) {
+//   const images = document.querySelectorAll(".image img");
+//   var modal = document.getElementById("Modal");
+//   var modalImg = document.getElementsByClassName("expandedImg");
+//   const modalTxt = document.querySelector("caption");
+//   modal.style.display = "block";
+//   document
+//     .getElementById("expandedImg")
+//     .setAttribute(
+//       "src",
+//       event.target.getAttribute("src"),
+//       event.target.getAttribute("alt")
+//     );
+//   captionText.innerHTML = this.alt;
+// }
 
-var modal = document.getElementById("myModal2");
-var img = document.getElementById("CS2");
-var modalImg = document.getElementById("CS02");
-var captionText = document.getElementById("caption2");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[1];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// function closeModal(event) {
+//   const modal = document.getElementById("myModal");
+//   modal.style.display = "none";
+// }
 
-var modal = document.getElementById("myModal3");
-var img = document.getElementById("CS3");
-var modalImg = document.getElementById("CS03");
-var captionText = document.getElementById("caption3");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[2];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+const image = document.querySelectorAll(".image img");
+const modal = document.querySelector(".modal");
+const expandedImg = document.querySelector(".expandedImg");
+const caption = document.querySelector(".caption");
+const closebtn = document.querySelector(".closebtn");
+image.forEach((image) => {
+  image.addEventListener("click", () => {
+    expandedImg.src = image.src;
+    caption.innerHTML = image.alt;
+    modal.classList.add("appear");
 
-var modal = document.getElementById("myModal4");
-var img = document.getElementById("CS4");
-var modalImg = document.getElementById("CS04");
-var captionText = document.getElementById("caption4");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[3];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal5");
-var img = document.getElementById("CPI1");
-var modalImg = document.getElementById("CPI01");
-var captionText = document.getElementById("caption5");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[4];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal6");
-var img = document.getElementById("CPI2");
-var modalImg = document.getElementById("CPI02");
-var captionText = document.getElementById("caption6");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[5];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal7");
-var img = document.getElementById("CPI3");
-var modalImg = document.getElementById("CPI03");
-var captionText = document.getElementById("caption7");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[6];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal8");
-var img = document.getElementById("CPI4");
-var modalImg = document.getElementById("CPI04");
-var captionText = document.getElementById("caption8");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[7];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal9");
-var img = document.getElementById("CPI5");
-var modalImg = document.getElementById("CPI05");
-var captionText = document.getElementById("caption9");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[8];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal10");
-var img = document.getElementById("CPI6");
-var modalImg = document.getElementById("CPI06");
-var captionText = document.getElementById("caption10");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[9];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal11");
-var img = document.getElementById("CPI7");
-var modalImg = document.getElementById("CPI07");
-var captionText = document.getElementById("caption11");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[10];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal12");
-var img = document.getElementById("CNPI1");
-var modalImg = document.getElementById("CNPI01");
-var captionText = document.getElementById("caption12");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[11];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal13");
-var img = document.getElementById("CNPI2");
-var modalImg = document.getElementById("CNPI02");
-var captionText = document.getElementById("caption13");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[12];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-var modal = document.getElementById("myModal14");
-var img = document.getElementById("CNPI3");
-var modalImg = document.getElementById("CNPI03");
-var captionText = document.getElementById("caption14");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[13];
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+    closebtn.addEventListener("click", () => {
+      modal.classList.remove("appear");
+    });
+  });
+});
